feb3e3e546e9982296a24593253c805bf0eae721
kommander-keene <keeneboondich@gmail.com>
2021-09-10 21:34:44 -0700

    commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -13,12 +13,24 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
 
+   this->Kp = Kp_;
+   this->Ki = Ki_;
+   this->Kd = Kd_;
+   this->p_error = 0.0;
+   this->i_error = 0.0;
+   this->d_error = 0.0;
+   this->prev_cte = 0.0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+   d_error = cte - prev_cte;
+   prev_cte = cte;
+   p_error = cte;
+   i_error += cte;
+
 
 }
 
@@ -26,5 +38,6 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+
+ return (-Kp * p_error) - (Ki * i_error) - (Kd * d_error);  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,8 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  pid.Init(0.15, 0.01, 2.5);
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -57,20 +57,27 @@ int main() {
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
+          double throttle = 0.3;
           /**
            * TODO: Calculate steering value here, remember the steering value is
            *   [-1, 1].
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+          // PID a;
+          // a.Init(0.1,0.1,0.1);
+          // a.UpdateError(speed);
+          // throttle = a.TotalError();
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value
                     << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -87,7 +94,7 @@ int main() {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
@@ -100,6 +107,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
